61f2220e49ff485029678c2063668f6ef32dbed9
ravikkat <52949827+ravikkat@users.noreply.github.com>
2022-07-12 09:49:24 +0800

    first commit

---
+++ Models/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetsAPI.Models
+{
+    public class Pet
+    {
+        public int PetId { get; set; }
+        public string PetName { get; set; }
+        public string Breed { get; set; }
+    }
+}
---
+++ Models/PetDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace PetsAPI.Models
+{
+   
+    public class PetDBContext : DbContext
+    {
+        public PetDBContext(DbContextOptions<PetDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pet> TodoItems { get; set; }
+    }
+}
